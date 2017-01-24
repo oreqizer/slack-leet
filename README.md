6dfe03dbb6157c837a6266184db51b81e9d1af33
oreqizer <oreqizer@gmail.com>
2017-01-24 14:52:37 +0100

    feat(app): Finish app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Slack 1337
+
+If using Heroku, see [this](http://stackoverflow.com/questions/33995194/what-timezone-is-heroku-server-using) for timezones.
--- app.go
+++ app.go
@@ -1,25 +1,31 @@
 package main
 
 import (
+	"github.com/robfig/cron"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 )
 
+
 const (
 	URL     = "https://slack.com/api/chat.postMessage"
-	token   = os.Getenv("SLACK_TOKEN")
-	channel = os.Getenv("SLACK_CHANNEL")
 	scope   = "chat:write:user"
 )
+
+var (
+	token   = os.Getenv("SLACK_TOKEN")
+	channel = os.Getenv("SLACK_CHANNEL")
+)
+
 
 func leet() {
 	var query url.Values
 	query.Add("token", token)
 	query.Add("channel", channel)
 	query.Add("scope", scope)
-	query.Add("as_user", true)
+	query.Add("as_user", "true")
 	query.Add("text", "13:37")
 
 	resp, err := http.NewRequest(http.MethodPost, URL+query.Encode(), nil)
@@ -29,5 +35,7 @@ func leet() {
 }
 
 func main() {
-
+	c := cron.New()
+	c.AddFunc("0 37 13 * * *", leet)
+	c.Start()
 }
