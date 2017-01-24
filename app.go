5c7f706fa645b0d7853520cfbaccda2f024ce2bd
oreqizer <oreqizer@gmail.com>
2017-01-24 14:31:17 +0100

    feat(app): Write leet function

---
+++ app.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+	"net/url"
+	"net/http"
+)
+
+const (
+	URL     = "https://slack.com/api/chat.postMessage"
+	token   = os.Getenv("TOKEN")
+	channel = os.Getenv("CHANNEL")
+	scope   = "chat:write:user"
+)
+
+
+func leet() {
+	var query url.Values
+	query.Add("token", token)
+	query.Add("channel", channel)
+	query.Add("scope", scope)
+	query.Add("as_user", true)
+	query.Add("text", "13:37")
+
+	resp, err := http.NewRequest(http.MethodPost, URL + query.Encode(), nil)
+	if err != nil {
+		log.Print("Error: " + err.Error())
+	}
+}
+
+func main() {
+
+}
