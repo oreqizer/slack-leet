5a212597527d367297c7d5bb529ef6d7775ec6c0
oreqizer <oreqizer@gmail.com>
2017-01-25 10:40:08 +0100

    refactor(app): Make it nicer

--- app.go
+++ app.go
@@ -8,56 +8,56 @@ import (
 	"os"
 )
 
-const (
-	URLpost   = "https://slack.com/api/chat.postMessage"
-	URLjoin   = "https://slack.com/api/channels.join"
-	joinScope = "channels:write"
-	postScope = "chat:write:user"
-)
-
 var (
 	token   = os.Getenv("SLACK_TOKEN")
 	channel = os.Getenv("SLACK_CHANNEL")
 )
 
+func join() {
+	query := url.Values{}
+	query.Add("token", token)
+	query.Add("name", channel)
+	query.Add("scope", "channels:write")
+
+	resp, err := http.PostForm("https://slack.com/api/channels.join", query)
+	if err != nil {
+		log.Printf("Error: %s\n", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error: %s\n", err.Error())
+		return
+	}
+	log.Printf("Join: Code: %d, Response: %s\n", resp.StatusCode, string(body))
+}
+
+func post() {
+	query := url.Values{}
+	query.Add("token", token)
+	query.Add("channel", channel)
+	query.Add("postScope", "chat:write:user")
+	query.Add("as_user", "true")
+	query.Add("text", "13:37")
+
+	resp, err := http.PostForm("https://slack.com/api/chat.postMessage", query)
+	if err != nil {
+		log.Printf("Error: %s\n", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error: %s\n", err.Error())
+		return
+	}
+	log.Printf("Post: Code: %d, Response: %s\n", resp.StatusCode, string(body))
+}
+
 func main() {
-	joinQuery := url.Values{}
-	joinQuery.Add("token", token)
-	joinQuery.Add("name", channel)
-	joinQuery.Add("scope", joinScope)
-
-	join, err := http.PostForm(URLjoin, joinQuery)
-	if err != nil {
-		log.Printf("Error: %s\n", err.Error())
-		return
-	}
-	defer join.Body.Close()
-
-	body, err := ioutil.ReadAll(join.Body)
-	if err != nil {
-		log.Printf("Error: %s\n", err.Error())
-		return
-	}
-	log.Printf("Join: Code: %d, Response: %s\n", join.StatusCode, string(body))
-
-	postQuery := url.Values{}
-	postQuery.Add("token", token)
-	postQuery.Add("channel", channel)
-	postQuery.Add("postScope", postScope)
-	postQuery.Add("as_user", "true")
-	postQuery.Add("text", "13:37")
-
-	post, err := http.PostForm(URLpost, postQuery)
-	if err != nil {
-		log.Printf("Error: %s\n", err.Error())
-		return
-	}
-	defer post.Body.Close()
-
-	body, err = ioutil.ReadAll(post.Body)
-	if err != nil {
-		log.Printf("Error: %s\n", err.Error())
-		return
-	}
-	log.Printf("Post: Code: %d, Response: %s\n", post.StatusCode, string(body))
+	join()
+	post()
 }
